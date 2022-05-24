cb31754a21792e68d3669d5da6f8691da4d9cf88
GHOST <ghostdevbusiness@gmail.com>
2022-05-25 00:56:50 +0100

    feat: 3d

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,39 +1,75 @@
 <script lang="ts">
     import {
-        CircleBufferGeometry,
-        MeshStandardMaterial,
-        BoxBufferGeometry,
-        DoubleSide,
-    } from 'three';
-    import {
         Canvas,
-        DirectionalLight,
-        HemisphereLight,
         Mesh,
         OrbitControls,
         PerspectiveCamera,
+        DirectionalLight,
+        AmbientLight,
+        HemisphereLight,
     } from 'threlte';
+    import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader.js';
+    import sviteBolt from '$lib/svite-bolt.svg?raw';
+    import { onMount } from 'svelte';
+    import * as THREE from 'three';
+
+    let sviteBoltGeometry: THREE.ExtrudeGeometry;
+
+    onMount(() => {
+        const [shape] = new SVGLoader()
+            .parse(sviteBolt)
+            .paths[0].toShapes(false);
+
+        sviteBoltGeometry = new THREE.ExtrudeGeometry(shape, {
+            curveSegments: 64,
+            depth: 2,
+            bevelEnabled: true,
+            bevelOffset: 0,
+            bevelSegments: 64,
+            bevelSize: 1,
+            bevelThickness: 2,
+        });
+
+        sviteBoltGeometry.center();
+    });
 </script>
 
 <div>
     <Canvas>
-        <PerspectiveCamera position={{ x: 10, y: 10, z: 10 }}>
-            <OrbitControls autoRotate />
+        <PerspectiveCamera position={{ x: 0, y: 0, z: -100 }}>
+            <OrbitControls autoRotate enableDamping />
         </PerspectiveCamera>
 
         <DirectionalLight
             shadow
-            color={'white'}
-            position={{ x: -15, y: 45, z: 20 }} />
+            color={'#EDBD9C'}
+            position={{ x: -20, y: 0, z: -100 }} />
+
         <HemisphereLight
-            skyColor={'white'}
-            groundColor={'#ac844c'}
-            intensity={0.4} />
+            skyColor={0x4c8eac}
+            groundColor={0xac844c}
+            intensity={0.6} />
+
+        {#if sviteBoltGeometry}
+            <Mesh
+                castShadow
+                geometry={sviteBoltGeometry}
+                position={{ x: 0, y: 0, z: 0 }}
+                rotation={{ x: 0, y: 0, z: 0 }}
+                material={new THREE.MeshStandardMaterial({
+                    color: new THREE.Color(0xff3e00),
+                    side: THREE.DoubleSide,
+                })} />
+        {/if}
 
         <Mesh
-            castShadow
-            geometry={new BoxBufferGeometry(1, 1, 1)}
-            material={new MeshStandardMaterial({ color: '#ff3e00' })} />
+            receiveShadow
+            rotation={{ x: Math.PI / 2, y: 0, z: 0 }}
+            geometry={new THREE.CylinderGeometry(12, 12, 2.5, 64)}
+            material={new THREE.MeshStandardMaterial({
+                color: new THREE.Color(0x222222),
+                side: THREE.DoubleSide,
+            })} />
     </Canvas>
 </div>
 
@@ -44,5 +80,6 @@
         left: 0;
         width: 100%;
         height: 100%;
+        background-color: #222;
     }
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,19 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter(),
+
+        vite: {
+            optimizeDeps: {
+                exclude: ['three'],
+            },
+        },
+    },
 };
 
 export default config;
