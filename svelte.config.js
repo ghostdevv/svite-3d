16592e994ff4a324686427faf0d6488932d95ad9
GHOST <ghostdevbusiness@gmail.com>
2022-12-14 15:05:59 +0000

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -9,12 +9,6 @@ const config = {
 
     kit: {
         adapter: adapter(),
-
-        vite: {
-            optimizeDeps: {
-                exclude: ['three'],
-            },
-        },
     },
 };
 
